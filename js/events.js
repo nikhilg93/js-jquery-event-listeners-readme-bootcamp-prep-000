9f1b85f69cbf0eea929c501e74530d5ee18e89ca
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-09-18 02:57:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ function pressIt(){
 
 function submitIt(){
   $('form').on('submit', function(){
-    alert("Y")
+    alert("Your form is going to be submitted now.")
   });
 }
 $(document).ready(function(){
