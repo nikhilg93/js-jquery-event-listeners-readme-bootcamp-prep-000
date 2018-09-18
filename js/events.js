0fddfcd3a6b8dac8f97f51c2948eb96c2e6f0480
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-09-18 03:02:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt(){
 function pressIt(){
   $('document').on('keydown', function(key){
     if(key.which == 71){
-      alert("G is pressed")
+      alert("G is pressed");
     }
     
   });
