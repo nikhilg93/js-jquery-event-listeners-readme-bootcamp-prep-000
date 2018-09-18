f9e19f28ded99433ff4b67c4b92da62fef46558e
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-09-18 02:59:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('document').on('keydown', function(){
+  $('document').on('keydown', function(key){
     if(key.which == 71){
       window.alert("G is pressed")
       return;
